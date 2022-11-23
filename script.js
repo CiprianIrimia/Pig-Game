be16f2935f3a6c728b815f4680483a9bafb1535b
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-11-23 20:40:00 +0200

    finalized

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let scores, currentScore, activePlayer, playing;
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+const resetGame = function () {
+  // Stored data
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  // Visible part
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  document.querySelector(`.player--0`).classList.remove("player--winner");
+  document.querySelector(`.player--1`).classList.remove("player--winner");
+  player0El.classList.add("player--active");
+};
+resetGame();
 
 // Create a switch to next player function
 const switchPlayer = function () {
@@ -54,15 +65,16 @@ btnRoll.addEventListener("click", function () {
   }
 });
 
+// Holding current score
 btnHold.addEventListener("click", function () {
   if (playing) {
-    // 1. Add current score to acive player's score
+    // 1. Add current score to active player's score
     scores[activePlayer] += currentScore; // scores[1] = scores[1] + currentScore
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100;
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 50) {
       //Finish the game
       diceEl.classList.add("hidden");
       playing = false;
@@ -78,3 +90,6 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+// Reset the game
+btnNew.addEventListener("click", resetGame);
